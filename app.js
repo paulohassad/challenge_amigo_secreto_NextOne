bd7629c5b1b8be90e4281150d063a9fbea3c1b20
paulohassad <pauloh.assad@gmail.com>
2025-09-02 14:51:57 -0300

    Criação da array e função

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Lista dos nomes de amigos inseridos no input
+let nomeAmigos = [];
+// Função para adicionar o nome na lista e verificar que esta sendo preenchido
+function adicionarAmigo(){
+    let nome = document.getElementById('amigo').value;
+  console.log(nome);
+  // Validação do nome, caso esteja vazio emite um alerta e retorna apenas quando for válido
+  if(nome === ""){
+    alert('Por favor, digite um nome no campo para prosseguir!')
+    return;
+  }
+  // Adiciona o nome a array
+  nomeAmigos.push(nome);
+  // Limpa o campo para o próximo nome
+  document.getElementById('amigo').value = "";
+}
+
+
